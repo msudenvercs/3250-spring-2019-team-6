ab7bd4163f1a96ec006796c98171f5bca3233f2a
Christian Waldron <cwaldro5@msudenver.edu>
2019-01-28 23:00:13 -0700

    first commit

---
+++ jvpm/HelloWorld.py
@@ -0,0 +1,8 @@
+def HelloWorld():
+
+    print("Hello world")
+
+    print("Hello Steven Wancewicz")
+    print("Hello Christian Waldron")
+if '__main__' == __name__:
+    HelloWorld()
---
+++ jvpm/test/test_HelloWorld.py
@@ -0,0 +1,14 @@
+#Team Members: Steven Wancewicz
+
+import unittest
+import sys
+import jvpm.HelloWorld
+from unittest.mock import Mock, call
+
+class TestHelloWorld(unittest.TestCase):
+    def test_HelloWorld(self):
+        sys.stdout = unittest.mock.Mock()
+        jvpm.HelloWorld.HelloWorld()
+        sys.stdout.assert_has_calls(
+            [call.write('Hello world'), call.write('\n'), call.write('Hello Steven Wancewicz'), call.write('\n')]
+        )
