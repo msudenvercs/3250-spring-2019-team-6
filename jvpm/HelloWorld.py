8d3831e5862cfc9dc4a371ef4c08838d707a0a1a
cwerner972 <46913629+cwerner972@users.noreply.github.com>
2019-01-30 13:11:29 -0700

    Create HelloWorld.py

---
+++ jvpm/HelloWorld.py
@@ -0,0 +1,9 @@
+def HelloWorld():
+
+    print("Hello world")
+    print("Hello Steven Wancewicz")
+    print("Hello Christian Waldron")
+    print("Hello Claire Wiesner")
+
+if '__main__' == __name__:
+    HelloWorld()
