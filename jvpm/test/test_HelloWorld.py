381b2ac70401833c78f7b269f3c83c0cc425c142
rykonermana <rykonermana@gmail.com>
2019-01-30 13:19:55 -0700

    Forgot to add my name to Team Members, it's there now

--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -1,6 +1,7 @@
 #Team Members: Steven Wancewicz
 #              Christian Waldron
 #              Claire Wiesner
+#              Daniel Reuter
 
 import unittest
 import sys
@@ -14,4 +15,4 @@ class TestHelloWorld(unittest.TestCase):
         sys.stdout.assert_has_calls(
             [call.write('Hello world'), call.write('\n'), call.write('Hello Steven Wancewicz'), call.write('\n'),
 			call.write('Hello Christian Waldron'), call.write('\n'), call.write('Hello Claire Wiesner'),
-                        call.write('\n'), call.write('Daniel Reuter'), call.write('\n')])
+                        call.write('\n'), call.write('Hello Daniel Reuter'), call.write('\n')])
