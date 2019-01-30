223aae0ea12db4d973c55b6cdbbf28a74c72e1bb
rykonermana <rykonermana@gmail.com>
2019-01-30 12:57:27 -0700

    Added my name to Hello World

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -3,6 +3,7 @@ def HelloWorld():
     print("Hello world")
     print("Hello Steven Wancewicz")
     print("Hello Christian Waldron")
+    print("Hello Daniel Reuter")
 
 if '__main__' == __name__:
     HelloWorld()
