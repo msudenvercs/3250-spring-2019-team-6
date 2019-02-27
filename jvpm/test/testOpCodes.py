a6d62d285e0ecc68ecb03b95a9c77c6bfa137040
Christian Waldron <cwaldro5@msudenver.edu>
2019-02-27 15:47:02 -0700

    Test for idiv

--- jvpm/test/testOpCodes.py
+++ jvpm/test/testOpCodes.py
@@ -8,3 +8,9 @@ class TestOpCodes(unittest.TestCase):
         self.assertEqual(OpCodes().interpret(0), 'not implemented')
         with self.assertRaises(KeyError):
             OpCodes().interpret(1)
+	def test_iadd_simple(self):
+	    test1 = OpCodes()
+		test1.stack.append(1)
+		test1.stack.append(2)
+		test1.idiv()
+		self.assertEqual(test1.pop(), 2)
