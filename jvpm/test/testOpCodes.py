25c6f00b2e70919e69e2341cda85fa014eb69726
Christian Waldron <cwaldro5@msudenver.edu>
2019-03-01 13:14:25 -0700

    added simple imul unittest

--- jvpm/test/testOpCodes.py
+++ jvpm/test/testOpCodes.py
@@ -8,9 +8,15 @@ class TestOpCodes(unittest.TestCase):
         self.assertEqual(OpCodes().interpret(0), 'not implemented')
         with self.assertRaises(KeyError):
             OpCodes().interpret(1)
-	def test_idiv_simple(self):
+    def test_idiv_simple(self):
 	    test1 = OpCodes()
-		test1.stack.append(1)
-		test1.stack.append(2)
-		test1.idiv()
-		self.assertEqual(test1.pop(), 2)
+	    test1.stack.append(2)
+	    test1.stack.append(4)
+	    test1.idiv()
+	    self.assertEqual(test1.stack.pop(), 2)
+    def test_imul_simple(self):
+	    test1 = OpCodes()
+	    test1.stack.append(2)
+		test1.stack.append(4)
+		test1.imul()
+		self.assertEqual(test1.stack.pop(), 8)
