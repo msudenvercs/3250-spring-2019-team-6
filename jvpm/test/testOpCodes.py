37c09d817dad8fc0688faf1135cdf3c3a86a9a57
cwerner972 <46913629+cwerner972@users.noreply.github.com>
2019-03-04 12:23:21 -0700

    first half of unittest opcodes and java test file

--- jvpm/test/testOpCodes.py
+++ jvpm/test/testOpCodes.py
@@ -8,3 +8,44 @@ class TestOpCodes(unittest.TestCase):
         self.assertEqual(OpCodes().interpret(0), 'not implemented')
         with self.assertRaises(KeyError):
             OpCodes().interpret(1)
+
+    def test_iadd_simple(self):
+	    testiadd = OpCodes()
+	    testiadd.stack.append(2)
+	    testiadd.stack.append(2)
+	    testiadd.iadd()
+	    self.assertEqual(testiadd.stack.pop(), 4) 
+    def test_iand_simple(self):
+	    testiand = OpCodes()
+	    testiand.stack.append(5)    
+	    testiand.stack.append(3)    
+	    testiand.iand()             
+	    self.assertEqual(testiand.stack.pop(), 1) 
+    def test_iconst_m1_simple(self):
+	    testiconst_m1 = OpCodes()       
+	    testiconst_m1.stack.append(-1)  
+	    self.assertEqual(testiconst_m1.stack.pop(), -1) 
+    def test_iconst_0_simple(self):
+	    testiconst_0 = OpCodes()
+	    testiconst_0.stack.append(0)    
+	    self.assertEqual(testiconst_0.stack.pop(), 0) 
+    def test_iconst_1_simple(self):
+	    testiconst_1 = OpCodes()
+	    testiconst_1.stack.append(1)    
+	    self.assertEqual(testiconst_1.stack.pop(), 1) 
+    def test_iconst_2_simple(self):
+	    testiconst_2 = OpCodes()
+	    testiconst_2.stack.append(2)    
+	    self.assertEqual(testiconst_2.stack.pop(), 2) 
+    def test_iconst_3_simple(self):
+	    testiconst_3 = OpCodes()
+	    testiconst_3.stack.append(3)    
+	    self.assertEqual(testiconst_3.stack.pop(), 3) 
+    def test_iconst_4_simple(self):
+	    testiconst_4 = OpCodes()
+	    testiconst_4.stack.append(4)    
+	    self.assertEqual(testiconst_4.stack.pop(), 4) 
+    def test_iconst_5_simple(self):
+	    testiconst_5 = OpCodes()
+	    testiconst_5.stack.append(5)   
+	    self.assertEqual(testiconst_5.stack.pop(), 5) 
