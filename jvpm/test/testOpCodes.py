340f67f7a19d4f2f3e72c481a1291d46bd8fe918
cwerner972 <46913629+cwerner972@users.noreply.github.com>
2019-03-04 12:31:11 -0700

    changed unittest for iconst

--- jvpm/test/testOpCodes.py
+++ jvpm/test/testOpCodes.py
@@ -23,29 +23,29 @@ class TestOpCodes(unittest.TestCase):
 	    self.assertEqual(testiand.stack.pop(), 1) 
     def test_iconst_m1_simple(self):
 	    testiconst_m1 = OpCodes()       
-	    testiconst_m1.stack.append(-1)  
+	    testiconst_m1.iconst_m1  
 	    self.assertEqual(testiconst_m1.stack.pop(), -1) 
     def test_iconst_0_simple(self):
 	    testiconst_0 = OpCodes()
-	    testiconst_0.stack.append(0)    
+	    testiconst_0.iconst_0    
 	    self.assertEqual(testiconst_0.stack.pop(), 0) 
     def test_iconst_1_simple(self):
 	    testiconst_1 = OpCodes()
-	    testiconst_1.stack.append(1)    
+	    testiconst_1.iconst_1    
 	    self.assertEqual(testiconst_1.stack.pop(), 1) 
     def test_iconst_2_simple(self):
 	    testiconst_2 = OpCodes()
-	    testiconst_2.stack.append(2)    
+	    testiconst_2.iconst_2   
 	    self.assertEqual(testiconst_2.stack.pop(), 2) 
     def test_iconst_3_simple(self):
 	    testiconst_3 = OpCodes()
-	    testiconst_3.stack.append(3)    
+	    testiconst_3.iconst_3    
 	    self.assertEqual(testiconst_3.stack.pop(), 3) 
     def test_iconst_4_simple(self):
 	    testiconst_4 = OpCodes()
-	    testiconst_4.stack.append(4)    
+	    testiconst_4.iconst_4    
 	    self.assertEqual(testiconst_4.stack.pop(), 4) 
     def test_iconst_5_simple(self):
 	    testiconst_5 = OpCodes()
-	    testiconst_5.stack.append(5)   
+	    testiconst_5.iconst_5   
 	    self.assertEqual(testiconst_5.stack.pop(), 5) 
